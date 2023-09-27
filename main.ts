1fc69947c89513d041177311ab45326c78499746
xudaashuai <xudashuai0819@outlook.com>
2023-09-27 06:00:04 +0000

    init

--- main.ts
+++ main.ts
@@ -1,26 +1,22 @@
 const TOKEN = "ppp"; // 替换为你在微信公众平台设置的 Token
-
-function checkSignature(
+import { encodeToString } from "https://deno.land/std/encoding/hex.ts";
+async function checkSignature(
   signature: string,
   timestamp: string,
   nonce: string
-): boolean {
+) {
   const arr = [TOKEN, timestamp, nonce].sort().join("");
-  const computedSignature = new TextEncoder().encode(arr);
-  const hash = new Uint8Array(20);
-  crypto.subtle.digest("SHA-1", computedSignature).then((digest) => {
-    hash.set(new Uint8Array(digest));
-  });
-  console.log(hash, signature);
-  return (
-    signature ===
-    Array.from(hash)
-      .map((b) => b.toString(16).padStart(2, "0"))
-      .join("")
+  const res = encodeToString(
+    new Uint8Array(
+      await crypto.subtle.digest("sha-1", new TextEncoder().encode(arr))
+    )
   );
+
+  console.log(res, signature);
+  return signature === res;
 }
 
-Deno.serve((req: Request) => {
+Deno.serve(async (req: Request) => {
   console.log(req);
   const url = new URL(req.url, `http://${req.headers.get("host")}`);
   const signature = url.searchParams.get("signature") || "";
@@ -29,7 +25,7 @@ Deno.serve((req: Request) => {
   const echostr = url.searchParams.get("echostr") || "";
 
   console.log(signature, timestamp, nonce, echostr);
-  if (checkSignature(signature, timestamp, nonce)) {
+  if (await checkSignature(signature, timestamp, nonce)) {
     return new Response(echostr);
   }
   return new Response();
